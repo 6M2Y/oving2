1e1695aa8f51029463d4b6e31f955203f51e2737
6m2y <minyahel.yimer@gmail.com>
2022-09-21 21:39:01 +0200

    third commit uten express router

---
+++ lists.js
@@ -0,0 +1,12 @@
+const lists = [
+    {
+        id: 1,
+        title: "Forberedelse (egenstudie)"
+    },
+    {
+        id : 2,
+        title : "programvaretesting"
+    }
+];
+
+export default lists;
---
+++ oppgaver.js
@@ -0,0 +1,31 @@
+const task =[
+    {
+        taskID : 1,
+        title:"Øving1-list1: les forberedelse sidene",
+        done: false,
+        listId : 1
+    },
+    {
+        taskID : 2,
+        title:"Øving2-list1: les forberedelse sidene",
+        done: false,
+        listId : 1
+    },
+    {
+        taskID : 1,
+        title:"Øving3-list2: lag test",
+        done: false,
+        listId : 2
+    },
+    {
+        taskID : 2,
+        title:"Øving4-list2: les forberedelse sidene",
+        done: false,
+        listId : 2
+    }
+];
+
+export default task;
+
+
+
